6cad63616b1f564874207f98e632477cb2f21182
Kyle Rodriguez <81923065+jimscreech@users.noreply.github.com>
2021-11-05 10:03:51 -0500

    Add files via upload

---
+++ STEMplained/app.js
@@ -0,0 +1,27 @@
+// window.onscroll = function() {
+//   scrollFunction()
+// };
+//
+// function scrollFunction() {
+//   if ((document.body.scrollTop > 65) || (document.documentElement.scrollTop > 65)) {
+//     // document.getElementById("brand").style.display = "none";
+//     // document.getElementById("mainNavbar").style.background = "rgb(255, 200, 50, 0.8)";
+//     document.getElementById("mainNavbar").style.borderBottom = "0.5px solid rgb(253, 255, 235, 0.2)";
+//
+//     // document.getElementById("brandLogo").style.width = "5rem";
+//     // document.getElementById("brandLogo").style.width= "3rem";
+//
+//     // document.getElementById("brandLogo").style.top = "1rem";
+//
+//   }else {
+//     // document.getElementById("brand").style.display = "inline";
+//     // document.getElementById("brandLogo").style.width = "5.5rem";
+//     // document.getElementById("brandLogo").style.width = "5.5rem";
+//
+//     document.getElementById("mainNavbar").style.borderBottom = "none";
+//     // document.getElementById("mainNavbar").style.background = "none";
+//     // document.getElementById("mainNavbar").style.color = "white"
+//     // document.getElementById("brandLogo").style.top = "0";
+//
+//   }
+// }
